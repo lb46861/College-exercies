5401d4c140932bb3f0ee443511772dc42cd87a80
Lovre Begovic <begovic.lovre@gmail.com>
2022-11-19 23:25:39 +0100

    github test

--- JavaScript Advanced Web Application Programming/exercise2/zad13.js
+++ JavaScript Advanced Web Application Programming/exercise2/zad13.js
@@ -2,4 +2,3 @@
 // funkciju i prosljediti joj imenicu. Unutar async funkcije koristiti ključnu riječ await kako bi se
 // dobio povrat iz ne-async-funkcije koja prima imenicu i konkatenira hardkodirani glagol i to vraća.
 // Povrat ispisati nakon tri sekunde.
-//sss
